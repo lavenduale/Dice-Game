85d44c45d755932fd97dc0ec6e9084858b873684
Yusong Du <yd3127@mun.ca>
2019-05-26 22:06:01 -0230

    JS game

--- app.js
+++ app.js
@@ -1,34 +1,136 @@
-/*
-GAME RULES:
+// ---------- Dice Game -------------- //
 
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
-
-*/
-
-let markS;
+let marks;
 let roundS;
 let activeP;
-let dice;
+let gameState;
 
-markS = [0, 0];
-roundS = 0;
-activeP = 'left';
+// initilize the data
+init();
 
-randomNum = Math.random();
-dice = Math.floor(6*randomNum)+1;
-//console.log(dice);
-
-document.querySelector('#current-' + activeP).textContent = dice;
+//document.querySelector('#current-' + activeP).textContent = dice;
 //document.querySelector('#current-' + activeP).innerHTML = '<em>' + dice + '</em>';
 
-// select and read html content
-const test = document.querySelector('#current-left').textContent;
-console.log(test);
+// Roll Button
+// event type + anonymous
+document.querySelector('.btn-roll').addEventListener('click', () => {
 
-//read css content
-document.querySelector('.dice').style.display = 'none';
+    if(gameState) {
 
+        randomNum = Math.random();
+        // I. Get a random number once clicking
+        let dice = Math.floor(6*randomNum)+1;
+        console.log(dice);
+
+        // II. Display the result
+        document.querySelector('.dice').style.display = 'block';
+        document.querySelector('.dice').src = 'dice-' + dice +'.png';
+
+        // III. Update the round score IF the rolled number was Not a 1
+        if (dice !== 1){
+            // add score
+            roundS += dice;
+            document.querySelector('#current-' + activeP).textContent =  roundS;
+        }
+        else {
+            // call next player
+            moveNext();
+        }
+    }
+});
+
+// Hold Button
+document.querySelector('.btn-hold').addEventListener('click', () => {
+
+    let indexAct;
+
+    if (gameState){
+        // I.   Add current score to global score (red area)
+        if (activeP === 'left'){
+            indexAct = 0;
+        }
+        else {
+            indexAct = 1; // right
+        }
+
+        marks[indexAct] += roundS; // score accmulating
+
+        // II.  Update the changes to UI
+        document.querySelector('#score-' + activeP).textContent = marks[indexAct];
+
+        // III. Check if Player wont the game
+        if (marks[indexAct] >= 25) {
+            document.querySelector('#name-' + activeP).textContent = 'Winner!'; // Change the score to Win!
+            document.querySelector('.dice').style.display = 'none';
+            // apply winner class from css to class list
+            document.querySelector('.player-'+ activeP).classList.add('winner');
+            // remove active spot
+            document.querySelector('.player-'+ activeP).classList.remove('active');
+            // set game to end
+            gameState = false;
+        }
+        else {
+            // call next player
+            moveNext();
+        }
+    }
+});
+
+// New Game Button
+document.querySelector('.btn-new').addEventListener('click', () => {
+    // reset the game to new
+    init();
+});
+
+// init function
+ function init() {
+    marks = [0, 0];
+    activeP = 'left';
+    roundS = 0;
+    gameState = true;
+
+    //read css content
+    document.querySelector('.dice').style.display = 'none';
+
+    // set up data
+    document.getElementById('score-left').textContent = '0';
+    document.getElementById('score-right').textContent = '0';
+    document.getElementById('current-left').textContent = '0';
+    document.getElementById('current-right').textContent = '0';
+
+    document.getElementById('name-left').textContent = 'Player Left';
+    document.getElementById('name-right').textContent = 'Player Right';
+
+    // remove winner class to restart
+    document.querySelector('.player-left').classList.remove('winner');
+    document.querySelector('.player-right').classList.remove('winner');
+    // remove active to restart
+    document.querySelector('.player-left').classList.remove('active');
+    document.querySelector('.player-right').classList.remove('active');
+
+    // set left player to be active
+    document.querySelector('.player-left').classList.add('active');
+}
+
+// move to next player function
+moveNext = () => {
+    // move active dot
+    if (activeP === 'left'){
+        activeP = 'right';
+    }
+    else {
+        activeP = 'left';
+    }
+    roundS = 0;
+
+    // reset the marks
+    document.getElementById('current-left').textContent = '0';
+    document.getElementById('current-right').textContent = '0';
+
+    // reset the active
+    document.querySelector('.player-left').classList.toggle('active');
+    document.querySelector('.player-right').classList.toggle('active');
+
+    // make the dice display when change to next player
+    document.querySelector('.dice').style.display = 'none';
+}
